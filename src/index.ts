254f9bbd536424f4e309073ac02c961e5cde720d
Antxony <dantonyofcarim@gmail.com>
2021-08-31 19:03:23 -0500

    hide menubar

--- src/index.ts
+++ src/index.ts
@@ -14,6 +14,7 @@ const createWindow = (): void => {
   const mainWindow = new BrowserWindow({
     height: 600,
     width: 800,
+    autoHideMenuBar: true,
   });
 
   // and load the index.html of the app.
